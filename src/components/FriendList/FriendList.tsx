f409f48e46e3969b3bbb802e33be2c68d52a2a61
NataliyaJilina <nataliyajilina@gmail.com>
2024-04-12 17:32:41 +0500

    social

--- src/components/FriendList/FriendList.tsx
+++ src/components/FriendList/FriendList.tsx
@@ -1,34 +1,18 @@
+import { AppHeading } from "../Typography/AppHeading"
+import { FriendListElem } from "./FriendListElem"
+import { dataFriend } from "./dataFriend"
+
 export const FriendList = () => {
     return(
         <div className="List">
           <div className="List__title">
-            <h2>Близкие друзья</h2>
-            <span className="count">123</span>
+            <AppHeading headingText="Близкие друзья" headingType="h2"/>
+            
+            <span className="count">{dataFriend.length}</span>
           </div>
-          <div className="UserElem">
-            <img src="./img/users/aleksandr-maykov.jpeg" alt="User" />
-            <div className="user__description">
-              <p className="main__text">Александр Майков</p>
-              <p className="secondary__text _online">Онлайн</p>
-            </div>
-            <span className="Badge">3</span>
-          </div>
-          <div className="UserElem">
-            <img src="./img/users/aleksandr-maykov.jpeg" alt="User" />
-            <div className="user__description">
-              <p className="main__text">Александр Майков</p>
-              <p className="secondary__text _online">Онлайн</p>
-            </div>
-            <span className="Badge">3</span>
-          </div>
-          <div className="UserElem">
-            <img src="./img/users/aleksandr-maykov.jpeg" alt="User" />
-            <div className="user__description">
-              <p className="main__text">Александр Майков</p>
-              <p className="secondary__text _online">Онлайн</p>
-            </div>
-            <span className="Badge">3</span>
-          </div>
+          {dataFriend && dataFriend.map((elem) => (
+           <FriendListElem imgLink={elem.imgLink} mainText={elem.mainText} secondaryText={elem.secondaryText} Badge={3}/> 
+          ))}
         </div>
     )
 }
