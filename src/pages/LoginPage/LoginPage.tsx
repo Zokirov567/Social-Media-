41d881195957bf3a8596ab137f3e41678a1b3d22
Xonjamil <xonjamil@gmail.com>
2024-04-15 20:34:52 +0500

    nn

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -5,20 +5,38 @@ import { AppInput } from "../../components/UI/AppInput/AppInput";
 import { LoginWith } from "../../components/LoginWith/LoginWith";
 import { AppHeading } from "../../components/Typography/AppHeading";
 import { SCLoginPage } from "./LoginPage.styled";
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+interface ILoginForm {
+  useremail: string;
+  password: string;
+}
+
+const loginFormSchema = yup.object({
+  useremail: yup.string().required("Обязательное поле"),
+  userpassword: yup
+    .string()
+    .required("Ведите пороль")
+    .min(8, "Не менее 8 символов"),
+});
 
 export const LoginPage = () => {
+
+  const {control, handleSubmit, fromState: {errors}} = useForm({resolver: yupResolver(loginFormSchema)})
+
   return (
     <SCLoginPage>
-      <AppHeading headingText="Авторизация" headingType={"h1"}/>
-      
+      <AppHeading headingText="Авторизация" headingType={"h1"} />
+
       <form action="#">
-        <AppInput inputType={"tel"} inputPlaceholder={"Номер телефона"}/>
-        <AppInput inputType={"password"} inputPlaceholder={"Пароль"}/>
-        
+        <AppInput inputType={"tel"} inputPlaceholder={"Номер телефона"} />
+        <AppInput inputType={"password"} inputPlaceholder={"Пароль"} />
+
         <Link to={"/main"}>
-        <AppButton buttonText={"Войти"}/>
+          <AppButton buttonText={"Войти"} />
         </Link>
-        
       </form>
       <Link to="#">Забыли пароль?</Link>
       <div className="registration">
@@ -26,8 +44,7 @@ export const LoginPage = () => {
           У вас нет аккаунта? <Link to="/registration">Зарегистрироваться</Link>
         </span>
         <p>Войти с помощью</p>
-        <LoginWith/>
-        
+        <LoginWith />
       </div>
     </SCLoginPage>
   );
